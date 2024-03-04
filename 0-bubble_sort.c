9fdf90b5a3af02652af8462292787240c90dc387
haythmelmagraby <haythmelmagraby@gmail.com>
2024-03-04 00:47:08 -0800

    0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,7 +7,7 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i = 0, n = 0;
+	size_t i = 0, n;
 	int store;
 
 	if (!array || !size)
@@ -15,13 +15,15 @@ void bubble_sort(int *array, size_t size)
 
 	while (i < size)
 	{
-		while (n < size)
+		n = 0;
+		while (n < size - 1)
 		{
 			if (array[n + 1] < array[n])
 			{
 				store = array[n];
 				array[n] = array[n + 1];
 				array[n + 1] = store;
+				print_array(array, size);
 			}
 			n++;
 		}
